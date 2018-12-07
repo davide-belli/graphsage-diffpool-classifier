3e4d7210567787626506470c435b5c90549029df
Davide <davidebelli95@gmail.com>
2018-12-07 15:03:46 +0100

    Initial GraphSAGE implementation

---
+++ source/models/graphsage.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class GraphSAGE(nn.Module):
+
+    def __init__(self, input_dim, output_dim, device="cuda:0"):
+        super(GraphSAGE, self).__init__()
+        self.device = device
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.linear = nn.Linear(self.input_dim, self. output_dim)
+
+    def aggregate_convolutional(self, x, a):
+        a += torch.eye(a.shape[0], device=self.device)
+        h_hat = a @ x
+        
+        return h_hat
+    
+    def forward(self, x, a):
+        h_hat = self.aggregate_convolutional(x, a)
+        h = F.relu(self.linear(h_hat))
+        h = F.normalize(h, p=2, dim=1)  # Normalize edge embeddings
+        
+        return h
