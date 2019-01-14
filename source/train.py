718bf3e2d61ba7f5a88f9628955cd4a4b887ae07
Davide <davidebelli95@gmail.com>
2019-01-14 17:47:59 +0100

    Added and executed training code

--- source/train.py
+++ source/train.py
@@ -6,6 +6,7 @@ import time
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import matplotlib.pyplot as plt
 from torch.nn import functional as F
 from torchvision.utils import make_grid
@@ -16,8 +17,44 @@ from models.graphsage import GraphSAGE
 from models.diffpool import DiffPool
 from utils.utils import *
 
-SEED = 42
+SEED = 422
 NORMALIZE = True
+
+def compute_accuracy(pred, target, device="cuda:0"):
+    pred_labels = torch.stack(pred, dim=0).to(device=device)
+    acc = (pred_labels.long() == target.long()).float().mean()
+    
+    return acc
+
+def run_epoch(classifier, optimizer, criterion, x_data, a_data, t_data, eval=False, device="cuda:0"):
+    data_len = x_data.size(0)
+    pred = []
+    losses = []
+    scores = []
+    if eval:
+        classifier.eval()
+    else:
+        classifier.train()
+        
+    for i in range(data_len):
+        optimizer.zero_grad()
+        x = x_data[i]
+        a = a_data[i]
+        t = t_data[i]
+        
+        _, _, y = classifier(x, a)
+        loss = criterion(y.unsqueeze(0), t.long().unsqueeze(0))
+        if not eval:
+            loss.backward()
+            optimizer.step()
+        pred.append(y.argmax())
+        losses.append(loss)
+        scores.append(y)
+    acc_epoch = compute_accuracy(pred, t_data, device=device).item()
+    loss_epoch = torch.FloatTensor(losses).mean().item()
+    
+    return acc_epoch, loss_epoch
+    
 
 def main():
     print(ARGS)
@@ -34,33 +71,72 @@ def main():
     dataset_helper.load_dataset(ARGS.dataset, device=device, seed=SEED, normalize=normalize)
     (x_train, a_train, labels_train) = dataset_helper.train
     (x_valid, a_valid, labels_valid) = dataset_helper.valid
-    feature_size = dataset_helper.feature_size
+    # feature_size = dataset_helper.feature_size
     print("Imported dataset, generated train and validation splits, took: {:.3f}s".format(time.time() - start_time))
 
-    x1 = x_train[0]
-    a1 = a_train[0]
-    t1 = labels_train[0]
-    t1_onehot = to_onehot_labels(t1)
+    # x1 = x_train[0]
+    # a1 = a_train[0]
+    # t1 = labels_train[0]
+    # # t1_onehot = to_onehot_labels(t1)
+    # classifier = Classifier(device=device).to(device=device)
+    # _ = classifier(x1, a1)
     
-    # Try GraphSAGE
-    gcn = GraphSAGE(feature_size, feature_size*2, device=device, normalize=normalize)
-    gcn = gcn.to(device=device)
-    gcn.train()
-    weights = gcn.linear.weight
-    z1 = gcn(x1, a1)
-    print()
-    
-    # Try DiffPool
-    diffpool = DiffPool(feature_size, x1.size(0)//2, device=device)
-    diffpool = diffpool.to(device=device)
-    diffpool.train()
-    x1_new, a1_new = diffpool(x1, a1)
-    print()
+    # # Try GraphSAGE
+    # gcn = GraphSAGE(feature_size, feature_size*2, device=device, normalize=normalize)to(device=device)
+    # gcn.train()
+    # weights = gcn.linear.weight
+    # z1 = gcn(x1, a1)
+    # print()
+    #
+    # # Try DiffPool
+    # diffpool = DiffPool(feature_size, x1.size(0)//2, device=device)to(device=device)
+    # diffpool.train()
+    # x1_new, a1_new = diffpool(x1, a1)
+    # print()
+    # input("remove test")
     
     # Try Classifier
-    classifier = Classifier(device=device)
-    classifier = classifier.to(device=device)
-    x_final, a_final, y_final = classifier(x1, a1)
+    
+    classifier = Classifier(device=device).to(device=device)
+    optimizer = optim.Adam(classifier.parameters())
+    criterion = nn.CrossEntropyLoss()
+    
+    assert x_train.size(0) == dataset_helper.train_size == labels_train.size(0)
+    assert x_valid.size(0) == dataset_helper.valid_size == labels_valid.size(0)
+    assert (labels_train.sum() + labels_valid.sum()) < 1000000
+    
+    print("\nStarted training")
+    acc_valid, loss_valid = run_epoch(classifier, optimizer, criterion, x_valid, a_valid, labels_valid, eval=True,
+                                      device=device)
+    print("Epoch {:.0f} | Acc (Valid): {:.3f} | Loss (Valid): {:.3f} |".format(0, acc_valid, loss_valid))
+    
+    measures = {
+        "acc" : {
+            "train" : [],
+            "valid" : []
+        },
+        "loss" : {
+            "train" : [],
+            "valid" : []
+        }
+    }
+    
+    for e in range(ARGS.epochs):
+        
+        acc_train, loss_train = run_epoch(classifier, optimizer, criterion, x_train, a_train, labels_train, eval=False, device=device)
+        acc_valid, loss_valid = run_epoch(classifier, optimizer, criterion, x_valid, a_valid, labels_valid, eval=True, device=device)
+        print("Epoch {:.0f} | Acc (Train/Valid): {:.3f}/{:.3f} | Loss (Train/Valid): {:.3f}/{:.3f} |"
+              .format(e + 1, acc_train, acc_valid, loss_train, loss_valid))
+        
+        measures["acc"]["train"].append(acc_train)
+        measures["acc"]["valid"].append(acc_valid)
+        measures["loss"]["train"].append(loss_train)
+        measures["loss"]["valid"].append(loss_valid)
+        
+        if e % 10 == 0:
+            for k in measures.keys():
+                generate_plot(measures[k]["train"], measures[k]["valid"], title=k)
+        
     print()
     
     
@@ -68,11 +144,11 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', default=10, type=int,
+    parser.add_argument('--epochs', default=100, type=int,
                         help='max number of epochs')
     parser.add_argument('--batch_size', default=16, type=int,
                         help='size of each batch')
-    parser.add_argument('--lr_rate', default=1e-3, type=float,
+    parser.add_argument('--lr_rate', default=1e-4, type=float,
                         help='learning rate')
     parser.add_argument('--device', default="cuda:0", type=str,
                         help='training device')
